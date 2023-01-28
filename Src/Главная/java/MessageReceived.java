02506b559a9c1c76f9d957a8a8bf3cdcf3a1cea5
Andrei <79273681+Ravskij@users.noreply.github.com>
2023-01-28 21:06:53 +0300

    Update MessageReceived.java

--- Src/Главная/java/MessageReceived.java
+++ Src/Главная/java/MessageReceived.java
@@ -2,6 +2,13 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
 public class MessageReceived extends ListenerAdapter {
     GuildCasino guildCasino = new GuildCasino();
     JoinToVoiceChannel joinToVoiceChannel = new JoinToVoiceChannel();
